36d3fe239bd4bddd46a446aa911f8757f6de62d3
devfajun@gmail.com <banji.liu@tuya.com>
2021-06-20 17:11:48 +0800

    feat: add log handler

--- httpz/handler/loghander.go
+++ httpz/handler/loghander.go
@@ -1,12 +1,14 @@
 package handler
 
 import (
+	"github.com/seepre/go-lamp/log"
 	"net/http"
 )
 
 func LogHandler(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// TODO write log
+		l := log.NewLog(log.DefaultLogger)
+		l.Info(r)
 		next.ServeHTTP(w, r)
 	})
 }
