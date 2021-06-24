88fea14dab4c54dba1ff31ea4700bb1573a9f0f5
devfajun@gmail.com <banji.liu@tuya.com>
2021-06-24 21:31:10 +0800

    fix: rename package

--- httpz/handler/loghander.go
+++ httpz/handler/loghander.go
@@ -1,13 +1,13 @@
 package handler
 
 import (
-	"github.com/seepre/go-lamp/log"
+	"github.com/seepre/go-lamp/logz"
 	"net/http"
 )
 
 func LogHandler(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		l := log.NewLog(log.DefaultLogger)
+		l := logz.NewLog(logz.DefaultLogger)
 		l.Info(r)
 		next.ServeHTTP(w, r)
 	})
