256f821e036f1258474bc8293aa478a0d4992617
班吉 <banji.liu@tuya.com>
2021-11-01 11:15:18 +0800

    add set an get

--- store/redisz/session.go
+++ store/redisz/session.go
@@ -1,6 +1,9 @@
 package redisz
 
-import "github.com/go-redis/redis"
+import (
+	"github.com/go-redis/redis"
+	"time"
+)
 
 type Session struct {
 	client *redis.Client
@@ -10,4 +13,17 @@ func NewSession(c Conf) Session {
 	return Session{
 		client: newRedis(c),
 	}
+}
+
+func (s *Session) SetWithExpireTime(key string, val string, expireT time.Duration) {
+	s.client.Set(key, val, expireT)
+}
+
+func (s *Session) Set(key string, val string) {
+	s.client.Set(key, val, -1)
+}
+
+func (s *Session) Get(key string) (string, error) {
+	ret := s.client.Get(key)
+	return ret.Result()
 }
